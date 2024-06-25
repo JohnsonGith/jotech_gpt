7772c726d43c0059bcb9377253e0321e1fcfd8a4
JohnsonGith <johnsonwebsax@gmail.com>
2024-06-25 03:27:26 +0200

    developping

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './navbar.css'
+import {RiMenu3Line, React.PropTypes} from 'react-icons/ri'
+import {FaSearch} from 'react-icons/fa'
+
+//BEM > Block Element Modifier
+//Block: navbar
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="gpt33__navbar">
+      <div classname="gpt33__navbar-links_container">
+        <div classname="gpt33__navbar-links_logo">
+          <img src="{logo}" alt="logo" />
+
+        </div>
+      </div>
       Navbar
     </div>
   )
