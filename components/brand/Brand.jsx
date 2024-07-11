6902b72bf05b6ccf5deb241895d27aae1a60f554
JohnsonGith <johnsonwebsax@gmail.com>
2024-07-11 23:41:59 +0200

    push mehn!

--- components/brand/Brand.jsx
+++ components/brand/Brand.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './brand.css'
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div className="gpt3__brand section__padding">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="brand__wrapper">
+              <img src={google} alt="Google" />
+              <img src={slack} alt="Slack" />
+              <img src={atlassian} alt="Atlassian" />
+              <img src={dropbox} alt="Dropbox" />
+              <img src={shopify} alt="Shopify" />
+            </div>
+          </div>
+        </div>
+
+    </div>
     </div>
   )
 }
